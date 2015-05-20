d8d5c73ab174e4b46fb20efc7f3655fcfd2f7af4
asantucci <santucci.andreas@gmail.com>
2015-05-19 18:00:00 -0700

    Now with a custom partitioner.

--- program/mc.scala
+++ program/mc.scala
@@ -3,6 +3,8 @@ import org.apache.spark.graphx.util.GraphGenerators
 import org.apache.spark._
 import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkContext._
+import org.apache.spark.Partitioner
 
 val N = 10
 val M = 10
@@ -18,16 +20,42 @@ def concatId(a: List[VertexId], b: List[VertexId]): List[VertexId] = {
     a ++ b
 }
 
-// Option 1: Create (Cluster, Subgraph) RDD, map(CC)
-// Option 2: Iteratively: CC(create(Subgraph))
+class customPartitioner extends Partitioner {
+  def numPartitions = B
+  def getPartition(key: Any): Int = key.asInstanceOf[Int]
+  override def equals(other: Any): Boolean = 
+    other.isInstanceOf[customPartitioner]
+}
 
-//var G = GraphGenerators.rmatGraph(sc, N, M)
 var G = GraphGenerators.logNormalGraph(sc, N, mu, sd)
 
 // sample edges with probability 1/e --> 1/c, where c ~ max degree.
 // To Do: Check bounds of P (e.g. what if 1?)
 val p_est: Double = 1.0 / G.degrees.reduce(min)._2
-val bc = sc.broadcast(p_est)
+//val bc = sc.broadcast(p_est)
+val v  = sc.broadcast(G.vertices)
+
+var sampled_edges: RDD[(Int, Edge[Int])] = sc.emptyRDD
+for (i <- 1 to B) {
+  sampled_edges = sampled_edges.union(
+                    G.edges
+                     .sample(false, p_est, i) // use of seed?
+                     .map(e => (i, e))
+                  )
+}
+
+sampled_edges = sampled_edges.partitionBy(new customPartitioner)
+sampled_edges.reduceByKey(
+
+// 0. Broadcast vertices to all machines.
+// 1. Create a RDD with key=machine.no, val = edge
+// 2. AggregateUsingIndex - Some function that takes a set of Edges
+//    creates sub-graph, runs CC, returns sorted CC-components...
+
+// Option 1: Create (Cluster, Subgraph) RDD, map(CC)
+// Option 2: Iteratively: CC(create(Subgraph))
+
+
 
 // Perform a Connected Components Analysis "on each machine"
 // To do this, generate B subgraphs, perform CC analysis,
@@ -40,7 +68,8 @@ for (i <- 1 to B) {
             .vertices
   cc.cache()
   ccs = ccs.union(
-         cc.aggregateUsingIndex(cc.map(pair => (pair._2, List(pair._1))), concatId)
+         cc.aggregateUsingIndex(cc.map(pair => (pair._2, List(pair._1))), 
+                                concatId)
            .map(p => p._2.sortWith(_ < _))) // can use(p._1, p._2.sortWith...) to track cc id.
 }
 
@@ -82,4 +111,6 @@ var t: RDD[(Int, org.apache.spark.graphx.Graph[Int, (Int, Int)])] = sc.emptyRDD
 for (i <- 1 to B) {
   t = t.union(G
 }
+
+//var G = GraphGenerators.rmatGraph(sc, N, M)
 
