f145205bb7fd39dc965a005dcbc54dac5f7c7baf
asantucci <santucci.andreas@gmail.com>
2015-05-24 11:04:15 -0700

    Rough draft of MST.

--- program/mst.scala
+++ program/mst.scala
@@ -7,10 +7,10 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.graphx.GraphOps
 //import scala.util.Sorting
 
-val N = 10
-val M = 10
-val mu = 2
-val sd = 1.5
+val N = 100
+val M = 100
+val mu = 20
+val sd = 4
 
 var G = GraphGenerators.logNormalGraph(sc, N, mu, sd)
 G.partitionBy(PartitionStrategy.EdgePartition2D) // Think!
@@ -20,20 +20,47 @@ G.partitionBy(PartitionStrategy.EdgePartition2D) // Think!
 
 //G.edges.flatMap{e => e.map(e.srcId -> e.attr)}
 
-case class EdgeWeight(dst: org.apache.spark.graphx.VertexId, wt: Int)
-case class EdgeWeight(arr: Iterable[(org.apache.spark.graphx.VertexId, Int)]) {
-  val edgeOrdering = Ordering.by {tuple => tuple._2}
+//case class EdgeWeight(dst: org.apache.spark.graphx.VertexId, wt: Int)
+//case class EdgeWeight(arr: Iterable[(org.apache.spark.graphx.VertexId, Int)]) {
+//  val edgeOrdering = Ordering.by {tuple => tuple._2}
+//}
+
+// Randomly weight edges.
+//G.edges.map{e => (e.srcId, e.dstId, (math.random*100).toInt)}
+
+var g = G.edges
+         .map(e => (e.srcId, (e.dstId, e.attr)))
+         .groupByKey  // No shuffle needed since edges partitioned ?
+         .map(pair => pair._1 ->
+              scala.collection.mutable.Queue(
+                pair._2
+                    .toList
+                    .sortBy{_._2}
+                    .take((math.log(N)/math.log(2)).toInt):_*)) // use of toList?
+         .collect // send back to driver...
+
+//def remove(torm:      (org.apache.spark.graphx.VertexId, Int),
+//           list: List[(org.apache.spark.graphx.VertexId, Int)]) = 
+//  list diff List(torm)
+
+//var mst = new Array[(Edge[Int])]((math.log(N)/math.log(2)).toInt + 1) // truncation?
+
+var mst = new Array[(Edge[Int])](N-1)
+var cc  = 0
+while (cc < N-1) {
+  var min = Double.PositiveInfinity
+  var idx = -1
+  for (i <- 0 to g.length-1) {
+    if (!g(i)._2.isEmpty && 
+         g(i)._2.head._2 < min) { // ith element, list element 0, item 2
+      min = g(i)._2.head._2
+      idx = i
+    }
+  }
+  var tmp = g(idx)._2.dequeue
+  mst(cc) = new Edge(g(idx)._1, tmp._1, tmp._2)
+  cc += 1
 }
-
-G.edges
- .map(e => (e.srcId, (e.dstId, e.attr)))
- .groupByKey  // No shuffle needed since edges partitioned ?
- .map(pair => 
-      (pair._1,
-       pair._2.toList.sortBy{_._2}.take(10))) // use of toList?
-
-
-
 
 ////////////////////
  .map{case (src, CompactBuffer(dst, wt)) => 
